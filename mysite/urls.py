dd8f00a7fe69287551f9806f4eceb8dce2dc142f
sgowris2 <sgowris2@gmail.com>
2016-09-10 12:47:59 +0530

    Url error fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^predictor/', include('predictor.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^predictor/$', include('predictor.urls')),
+    url(r'^admin/$', include(admin.site.urls)),
 )
