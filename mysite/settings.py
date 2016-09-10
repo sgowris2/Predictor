450ee78b159ea151d98b45598a53c977ad362211
sgowris2 <sgowris2@gmail.com>
2016-09-10 12:53:35 +0530

    Still trying to fix url errors

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'predictor'
+    'predictor',
 )
 
 MIDDLEWARE_CLASSES = (
