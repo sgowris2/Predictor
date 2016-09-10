7ec9736ac63561c835837a36939dfb6209b8195c
sgowris2 <sgowris2@gmail.com>
2016-09-10 13:48:23 +0530

    Redirecting Root to Predictor App

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^/', include('predictor.urls')),
+    url(r'^', include('predictor.urls')),
     url(r'^predictor/', include('predictor.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
