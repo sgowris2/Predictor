669cfa2f01ad7d1167cffd81f7d7a0ebf4c29616
sgowris2 <sgowris2@gmail.com>
2016-09-10 12:58:23 +0530

    reverting and trying again

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', include('predictor.urls')),
-    url(r'^admin/$', include(admin.site.urls)),
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^predictor/', include('predictor.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
