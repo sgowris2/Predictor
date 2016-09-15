bb73f2866350c93e855697b036e6a7ea3d6cb65e
sgowris2 <sgowris2@gmail.com>
2016-09-16 00:16:24 +0530

    Fixing print statement

--- predictor/utilities.py
+++ predictor/utilities.py
@@ -16,7 +16,6 @@ def contact_timeout_check(user):
     try:
         now = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
         cutoff_time = (now - datetime.timedelta(minutes=15))
-        print cutoff_time
         if len(FeedbackMessage.objects.filter(user=user, timestamp__gt=cutoff_time)) > 0:
             return False
         else:
